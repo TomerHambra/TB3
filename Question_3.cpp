2205c95786e01488852b3386834df1b07d166524
Tomer Hambra <73949014+TomerHambra@users.noreply.github.com>
2024-05-06 00:48:57 +0300

    starting 2-3 tree

--- Question_3.cpp
+++ Question_3.cpp
@@ -15,7 +15,6 @@
                 C  O  N  T  A  I  N  E  R      T  Y  P  E  S
 ****************************************************************************/
 
-
 typedef struct Record{
     int serial_number;
     int price;
@@ -26,79 +25,6 @@ typedef struct Record{
     }
 } Record;
 
-
-typedef struct PriceNode {
-    int price;
-    avl_tree<Record> * records_tree;
-
-    PriceNode(int price) : price(price), records_tree(nullptr) {
-        records_tree = new avl_tree<Record>();
-    };
-
-    ~PriceNode(){
-        delete records_tree;
-    }
-
-    bool operator<(PriceNode& other){
-        return price < other.price;
-    }
-} PriceNode;
-
-/**---------------------- Helper functions -------------------------------**/
-
-void update(avl_tree_node<Record> * node){
-    auto max_price = node->value;
-    if(node->left != nullptr) max_price = node->left->value.price < node->value.price ? node->value : node->left->value;
-    if(node->right != nullptr) max_price = node->right->value.price < node->value.price ? node->value : node->right->value;
-    node->_max = max_price;
-}
-/**------------------------------- & -------------------------------------**/
-
-typedef struct SeriesElements {
-    /** Since the data structure from question 1 is helpful here, were just going to copy the code from there **/
-
-    avl_tree<Record> tree1{&update};
-    Record max_price = {-1, -1, ""};
-    int next_serial_number = 1;
-
-
-    SeriesElements() = default;
-    ~SeriesElements() = default;
-
-    void Init(){
-        max_price = {-1, -1, ""};
-        next_serial_number = 1;
-    }
-
-    void Insert(int price, std::string name){
-        Record record = {next_serial_number++, price, name};
-        tree1.insert(record);
-    }
-
-    void Delete(int number){
-        // keep in mind that the search is done only using the serial number
-        Record dummy = {number, -1, ""};
-        auto temp = tree1.find(dummy);
-        if(temp == nullptr) return; // no need to delete something that is not in the trees.
-        Record record = temp->value;
-
-        tree1.remove(record);
-        max_price = tree1.root->_max;
-    }
-
-    Record Max_Price() {
-        if(max_price.serial_number == -1)
-            std::cout << "No elements in the data structure, so default max_price is returned." << std::endl;
-        return this->max_price;
-    }
-} SeriesElements;
-
-
-typedef struct SeriesNode {
-
-
-
-} SeriesNode;
 
 
 
